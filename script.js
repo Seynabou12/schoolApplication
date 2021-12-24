b9a0de549fee685d8ba457bb35e13169c555d859
Seynabou12 <dioneseynabou0@gmail.com>
2021-12-24 16:42:35 +0100

    modification de la carte

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdC
 const API_URL = "https://begawfkjokyyfiqpcwzp.supabase.co/rest/v1/Apprenant";
 
 //Récuperer les éléments du DOM
+let erreur = ""
 const listApprenant = []
 const positionElement = document.getElementById("formulaire")
 const inputNom = document.querySelector("#nom")
@@ -17,11 +18,32 @@ const enregistrer = document.querySelector("#sauvegarde")
 // RECUPERATION DES INFORMAIONS DU FORMULAIRE
 positionElement.addEventListener("submit", (event) => {
     event.preventDefault()
-    if (inputNom.value.trim() == "" ) {
+    if (inputNom.value.trim() == "" || inputPrenom.value.trim() == "" || 
+    inputEmail.value.trim() == "") {
       const error = document.getElementById("error")
       error.innerHTML = "Tous les champs sont requis"
       error.style.color = "red"
     }
+    // if (!inputBiographie.value) {
+    //   erreur = "Veuillez renseigner une biographie"
+    // }
+    // if (!inputEmail.value) {
+    //   erreur = "Veuillez renseigner un email"
+    // }
+    // if (!inputPrenom.value) {
+    //   erreur = "Veuillez renseigner un prenom"
+    // }
+    // if (!inputNom.value) {
+    //   erreur = "Veuillez renseigner un nom"
+    // }
+    // if (erreur) {
+    //   event.preventDefault()
+    //   document.getElementById('erreur').innerHTML = erreur
+    //   return false;
+    // }
+    // else{
+    //   alert("Apprenant ajouter avec succé")
+    // }
   // Récupération des informations saisies
     const nomSaisi = inputNom.value
     const prenomSaisi = inputPrenom.value
@@ -36,91 +58,56 @@ positionElement.addEventListener("submit", (event) => {
       niveau:niveauSaisi
     }
     carteApprenant(newApprenant)
-    
+    positionElement.reset()
   })
-
   function carteApprenant (apprenant){
     let id = Math.random().toString()
     sauvegarde.insertAdjacentHTML("beforeend", `
         <div class="card mt-3" id="${id}">
-                <div class="profil d-flex justify-content-evenly ">
-                    <img src="./images/image.jpg" alt="" srcset="">
-                    <div class="edit">
-                    <i class="bi bi-pencil"></i>
-                    </div>
-                    <div class="trash-icon">
-                    <i class="bi bi-trash"></i>
-                    </div>
-                </div>
-                <div class="profil-main">
-                <h6 class="profil-nom">${apprenant.nom} ${apprenant.prenom}</h6>
-                <p class="email">${apprenant.email}</p>
-                <p class="profil-body">${apprenant.biographie}</p>
-                <br>
-                <p class="profil-position">${apprenant.niveau}</p>
-                </div>
+            <div class="profil d-flex justify-content-evenly ">
+              <img src="./images/image.jpg" alt="" srcset="">
+              <div class="edit">
+              <i class="bi bi-pencil"></i>
+              </div>
+              <div class="trash-icon">
+              <i class="bi bi-trash"></i>
+              </div>
+            </div>
+            <div class="profil-main">
+            <h6 class="profil-nom" data-nom="${apprenant.nom}" data-prenom="${apprenant.prenom}" >${apprenant.nom} ${apprenant.prenom}</h6>
+            <p class="email">${apprenant.email}</p>
+            <p class="profil-body">${apprenant.biographie}</p>
+            <br>
+            <p class="profil-position">${apprenant.niveau}</p>
+            </div>
         </div>`)
-  
-        //supprimer une carte ajouter
+        //Suppression des cartes en local
         let card = document.querySelector(`.card[id="${id}"]`)
-        const supprimer = card.querySelector(".bi-trash")
-
-        //Modification de la carte en local
-        const modifier = document.querySelector(".bi-pencil")
-        supprimer.addEventListener('click', (e) =>{
+        const supprimer = card.querySelector(".bi-trash") 
+        supprimer.addEventListener('click', (e) => {
           e.target.parentElement.parentElement.parentElement.remove()
         })
+        
+        //Modification de la carte en local
+        const modifier = card.querySelector(".bi-pencil")
         modifier.addEventListener('click', (event) => {
+
+          const nom = card.querySelector(".profil-nom").dataset.nom
+          const prenom = card.querySelector(".profil-nom").dataset.prenom
+          const email = card.querySelector(".email").textContent
+          const bio = card.querySelector(".profil-body").textContent
+          const niveau = card.querySelector(".profil-position").textContent
           
-        })
+          inputNom.value=nom
+          inputPrenom.value=prenom
+          inputEmail.value=email
+          inputBiographie.value=bio
+          inputNiveau.value=niveau
+        }) 
       }
-      // function updateOrDeleteApprenant(){
-      //     const deleteButton = document.querySelectorAll(".bi-trash")
-      //     //console.log(deleteButton);
-      //     const editButton = document.querySelectorAll(".bi-pencil")
-      //     deleteButton.forEach((button) => {
-      //       button.addEventListener('click', () => deleteApprenant(button.id))
-            
-      //     })
-      //     editButton.forEach((button) => {
-      //       button.addEventListener('click', () => editApprenant(button.id))
-      //       alert("bonjout tout le monde")
-      //     })
-      // }
-
-
-      // function deleteApprenant(id) {
-      //   listApprenant.forEach((apprenant) => {
-      //     const positionApprenant = listApprenant.indexOf(apprenant)
-      //     apprenant.id === parseInt(id) && listApprenant.splice(positionApprenant, 1)
-      //   })
-      // listApprenant.forEach((apprenant) => {
-      //   if (apprenant.id === parseInt(id)) {
-      //       apprenant.nom = newInput.nom,
-      //       apprenant.prenom = newInput.prenom,
-      //       apprenant.email = newInput.email,
-      //       apprenant.biographie = newInput.biographie,
-      //       apprenant.niveau = newInput.niveau
-      //   }
-      //   carteApprenant()
-      // }, 
-
-      // modifier.addEventListener("click", (event) => {
-      //   const newInput = {
-      //     nom : document.getElementById(`NomOfApprenant${id}`),
-      //     prenom : document.getElementById(`PrenomOfApprenant${id}`)
-      //   }
-      //   console.log(newInput)
-      //     apprenant.forEach((apprenant) => {
-      //       if (apprenant.id === parseInt(id)) {
-      //         apprenant.nom = newInput.nom
-      //         // apprenant.prenom = newInput.prenom,
-      //         // apprenant.email = newInput.email,
-      //         // apprenant.niveau = newInput.niveau,
-      //         // apprenant.biographie = newInput.biographie
-      //       }
-      //     })
-      // })
+  
+   
+          
       
       
 
