93d2a279ab55ca6e9cef81609d886fcd5dc1702a
Seynabou12 <dioneseynabou0@gmail.com>
2021-12-27 19:47:58 +0100

    redirection à la liste des apprenants

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTU4OTY2NiwiZXhwIjoxOTU1MTY1NjY2fQ.61uxHqbNW70gKjOYqYVZ_xrG7sZlocp8vy37SSf2KHM"
-const API_URL = "https://begawfkjokyyfiqpcwzp.supabase.co/rest/v1/Apprenant";
+const API_URL = "https://begawfkjokyyfiqpcwzp.supabase.co/rest/v1/Apprenant"
 
 //Récuperer les éléments du DOM
 var error
 const listApprenant = []
 const positionElement = document.getElementById("formulaire")
-// console.log(positionElement);
 const inputNom = document.querySelector("#nom")
 const inputPrenom = document.querySelector("#prenom")
 const inputEmail = document.querySelector("#email")
 const inputNiveau = document.querySelector("#niveau")
 const inputBiographie = document.querySelector("#biographie")
-const inputPhoto = document.querySelector("photo")
+const inputPhoto = document.querySelector("#photo")
 const carteContainer = document.querySelector(".carte")
 const sauvegarde = document.querySelector(".save")
 const enregistrer = document.querySelector("#sauvegarde")
+const ajout = document.querySelector(".btn-ajout")
+const btnModifier = document.querySelector(".btn-edit")
+const redirection =  document.querySelector(".liste")
 
 
 // RECUPERATION DES INFORMATIONS DU FORMULAIRE
@@ -43,27 +45,22 @@ positionElement.addEventListener("submit", (event) => {
     carteApprenant(newApprenant)
     positionElement.reset()
   })
-
   //Affichage de la carte
   function carteApprenant (apprenant){
     let id = Math.random().toString()
     sauvegarde.insertAdjacentHTML("beforeend", `
-        <div class="card mt-3" id="${id}">
-            <div class="profil d-flex justify-content-evenly ">
-              <img src="./images/image.jpg" alt="" srcset="">
-              <div class="edit">
-              <i class="bi bi-pencil"></i>
-              </div>
-              <div class="trash-icon">
-              <i class="bi bi-trash"></i>
-              </div>
-            </div>
-            <div class="profil-main">
-            <h6 class="profil-nom" data-nom="${apprenant.nom}" data-prenom="${apprenant.prenom}" >${apprenant.nom} ${apprenant.prenom}</h6>
-            <p class="email">${apprenant.email}</p>
-            <p class="profil-body">${apprenant.biographie}</p>
-            <br>
-            <p class="profil-position">${apprenant.niveau}</p>
+        <div class="card mt-3  mb-3" id="${id}">
+          <div class="profil mt-2 ">
+              <p><img src="./images/nabou.png" alt="" srcset="" class="photo">${apprenant.photo}</p>
+              <img src="./images/pencil.png" class="bi bi-pencil">
+              <div class="trash-icon"><img src="./images/trash.png" class="bi bi-trash"></div>
+          </div>
+            <div class="card-body profil-main">
+              <h6 class="profil-nom" data-nom="${apprenant.nom}" data-prenom="${apprenant.prenom}" ><span class="app-nom">${apprenant.nom}</span> <span class="app-prenom">${apprenant.prenom}</span></h6>
+              <p class="email">${apprenant.email}</p>
+              <p class="profil-body">${apprenant.biographie}</p>
+              <br>
+              <p class="profil-position">${apprenant.niveau}</p>
             </div>
         </div>`)
 
@@ -72,23 +69,48 @@ positionElement.addEventListener("submit", (event) => {
         const supprimer = card.querySelector(".bi-trash") 
         supprimer.addEventListener('click', (e) => {
           e.target.parentElement.parentElement.parentElement.remove()
+          if(confirm("voulez-vous vraiment supprimer cette carte")){}
         })
         
           //Modification de la carte en local
           const modifier = card.querySelector(".bi-pencil")
           modifier.addEventListener('click', (event) => {
-
+          ajout.classList.add("cacher")
+          btnModifier.classList.remove("cacher")
+          if(confirm("voulez-vous vraiment modifier cette carte")){}
           const nom = card.querySelector(".profil-nom").dataset.nom
           const prenom = card.querySelector(".profil-nom").dataset.prenom
+          const appNom = card.querySelector(".app-nom")
+          const appPrenom = card.querySelector(".app-prenom")
           const email = card.querySelector(".email").textContent
+          const Email = card.querySelector(".email")
+          const Bio = card.querySelector(".profil-body")
           const bio = card.querySelector(".profil-body").textContent
           const niveau = card.querySelector(".profil-position").textContent
+          const Niveau = card.querySelector(".profil-position")
           
-          inputNom.value=nom
-          inputPrenom.value=prenom
-          inputEmail.value=email
-          inputBiographie.value=bio
-          inputNiveau.value=niveau
+          inputNom.value = nom
+          inputPrenom.value = prenom
+          inputEmail.value = email
+          inputBiographie.value = bio
+          inputNiveau.value = niveau
+          // inputPhoto.value = photo
+
+          btnModifier.addEventListener("click", (event) => {
+            btnModifier.classList.add("cacher")
+            ajout.classList.remove("cacher")
+            apprenant.nom = inputNom.value
+            apprenant.prenom = inputPrenom.value
+            apprenant.email = inputEmail.value
+            apprenant.biographie = inputBiographie.value
+            apprenant.niveau = inputNiveau.value
+
+            appNom.textContent = apprenant.nom
+            appPrenom.textContent =  apprenant.prenom
+            Email.textContent = apprenant.email
+            Bio.textContent = apprenant.biographie
+            Niveau.textContent = apprenant.niveau
+          })
         }) 
       }
 
@@ -102,7 +124,6 @@ positionElement.addEventListener("submit", (event) => {
     //actualiser le dom pour afficher le nombre
     const paragraph = document.getElementById("limit")
     const text = document.getElementById("progress")
-    const ajout = document.querySelector(".btn-ajout")
     text.textContent = longueurSaisi
 
     if (reste <= 5) {
@@ -112,5 +133,18 @@ positionElement.addEventListener("submit", (event) => {
       paragraph.style.color = "#00000"
       ajout.disabled = false
     }
-
   })
+
+  //Enregistrement au niveau de SUPABASE
+//   enregistrer.addEventListener("click", (event) => {
+//     event.preventDefault()
+//     //ENVOYER LES DONNEES VERS SUPABASE
+//     fetch(API_URL, {
+//     method: "POST",
+//     headers: {
+//       apikey: API_KEY,
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newApprenant)
+//   })
+// })
